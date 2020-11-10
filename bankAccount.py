43472591ef3af7bbf28b26207cc7c1d9b3dd23cf
Venturing Guy <LVentura335@Gmail.com>
2020-11-09 21:48:34 -0800

    Create bankAccount.py
    first commit

---
+++ bankAccount.py
@@ -0,0 +1,22 @@
+class BankAccount():
+  def __init__(self, full_name, account_number, routing_number, balance):
+    self.full_name = full_name
+    self.account_number = account_number
+    self.routing_number = routing_number
+    self.balance = balance
+
+  def deposit():
+    # define deposit method here
+
+  def withdraw():
+    # define withdraw method here
+  
+  def get_balance():
+    #define get_balance method here 
+
+  def add_interest():
+    # define get_interest method here 
+
+  def print_receipt():
+    # define print_receipt method here 
+    
